e915cbc83102e92641214aeefacbc9e21da1c698
flamenkito <flamenkito@users.noreply.github.com>
2026-01-12 16:07:23 +0100

    chore: initial commit

---
+++ src/json-rpc.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import {
+  JsonRpcHandler,
+  JsonRpcRequest,
+  JsonRpcResponse,
+} from './json-rpc.handler';
+
+@Controller('rpc/v1')
+export class JsonRpcController {
+  constructor(private readonly rpcHandler: JsonRpcHandler) {}
+
+  @Post()
+  async handleRpc(@Body() request: JsonRpcRequest): Promise<JsonRpcResponse> {
+    return this.rpcHandler.handle(request);
+  }
+}
